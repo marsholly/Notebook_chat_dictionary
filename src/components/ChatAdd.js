6bfee59c986caaefa519ba590d6ccdeb2951ac13
Hyein <hyeinu65@gmail.com>
2016-08-20 17:18:29 -0700

    working on chat capabilities

--- src/components/ChatAdd.js
+++ src/components/ChatAdd.js
@@ -10,30 +10,19 @@ export default class NoteAdd extends Component {
       username: '',
       message: ''
     }
-
-    this.changeUsername = this.changeUsername.bind(this)
     this.changeMessage = this.changeMessage.bind(this)
     this.submitMessage = this.submitMessage.bind(this)
-    this.setUser = this.setUser.bind(this)
-
   }
 
-  changeUsername(e){
-    this.setState({user_name: e.target.value})
-  }
-  setUser(e){
-    e.preventDefault();
-    this.setState({username: this.state.user_name})
-  }
+
   changeMessage(e){
       this.setState({message: e.target.value})
   }
   submitMessage(e){
     e.preventDefault();
-    // let newMessage = "@" + this.state.username + ":" + this.state.message
 
     const message = {
-      user_name: this.state.username,
+      user_name: this.props.username,
       message: this.state.message,
       time_stamp: moment()
     }
@@ -42,25 +31,14 @@ export default class NoteAdd extends Component {
   }
 
   render(){
-    let username
     let user
-    if(this.state.username){
-      username = (<h2></h2>)
-      user = "@" + this.state.username
+    if(this.props.username){
+      user = "@" + this.props.username
     } else{
-      username = (
-        <div className="input-group form-inline">
-          <input type="text" className="form-control" placeholder="Enter Username" value={this.state.user_name} onChange={this.changeUsername}/>
-          <a className="input-group-addon" >
-          <button className="btn btn-info btn-xs" type="submit" onClick={this.setUser}>Enter</button></a>
-        </div>
-      )
       user = ""
     }
     return (
       <div>
-        <h1>Chat</h1>
-        {username}
         <div className="input-group">
           <input id="btn-input" type="text" className="form-control input-md" placeholder={user} onChange={this.changeMessage} />
             <span className="input-group-btn">
@@ -71,5 +49,3 @@ export default class NoteAdd extends Component {
     )
   }
 }
-
-{/* <textarea className="form-control" placeholder={user} rows="2" value={this.state.message} onChange={this.changeMessage}></textarea> */}
--- src/components/ChatBoard.js
+++ src/components/ChatBoard.js
@@ -7,9 +7,14 @@ export default class ChatBoard extends Component {
   constructor(){
     super()
     this.state ={
-      messages: ChatStore.getAll()
+      messages: ChatStore.getAll(),
+      user_name: '',
+      username: ''
     }
     this._onChange = this._onChange.bind(this)
+    this.setUser = this.setUser.bind(this)
+    this.changeUsername = this.changeUsername.bind(this)
+
   }
   componentDidMount(){
     ChatStore.startListening(this._onChange);
@@ -20,13 +25,38 @@ export default class ChatBoard extends Component {
   _onChange(){
     this.setState({messages: ChatStore.getAll()})
   }
+  changeUsername(e){
+    this.setState({user_name: e.target.value})
+  }
+  setUser(e){
+    e.preventDefault();
+    this.setState({username: this.state.user_name})
+  }
+
   render(){
+    let username
+    if(this.state.username){
+      username = (
+        <ChatAdd username={this.state.username}/>
+      )
+    } else {
+      username = (
+        <div className="input-group form-inline">
+          <input type="text" className="form-control" placeholder="Enter Username" value={this.state.user_name} onChange={this.changeUsername}/>
+          <span className="input-group-btn">
+            <button className="btn btn-info btn-md" id="btn-chat" onClick={this.setUser}>Enter</button>
+          </span>
+        </div>
+      )
+    }
     let messages = this.state.messages.map((message, index) =>{
       return <Chat key={index} message={message}/>
     })
     return(
       <div>
-        <ChatAdd />
+        <h1>Chat</h1>
+        {username}
+
         <table className="table">
           <tbody>
             {messages}
